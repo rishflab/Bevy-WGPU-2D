22ab91a0a08ed40fe4732802995a69c0f4c96153
Nishad-Sharma <Nishad.sharma@hotmail.com>
2021-04-17 16:18:34 +1000

    Extend sprite asset json
    Added a file format which improves flexibility when specifying animation
    frames. Previously our loading function only worked on animation strips.
    
    Now we source animation frames from multiple files with custom image
    sizes.
    
    We also removed hardcoded ranges required to access animations.

--- src/sprite.rs
+++ src/sprite.rs
@@ -1,54 +1,67 @@
 use crate::TEXTURE_ARRAY_SIZE;
 use arrayvec::ArrayVec;
 use serde::Deserialize;
-use std::ops::Range;
+use std::path::PathBuf;
 
-#[derive(Deserialize, Debug, Copy, Clone)]
+#[derive(Deserialize, Debug, Clone)]
 pub struct KeyFrame {
-    pub index: u8,
+    pub png: PathBuf,
     pub time: f32,
-}
-
-pub struct Sprite {
-    pub id: usize,
-    pub frame_id: u8,
+    pub view: View,
 }
 
 #[derive(Deserialize, Debug, Clone)]
-pub struct AnimTimeline(pub ArrayVec<KeyFrame, TEXTURE_ARRAY_SIZE>);
+pub struct View {
+    pub x: u32,
+    pub y: u32,
+    pub width: u32,
+    pub height: u32,
+}
+
+#[derive(Clone, Copy)]
+pub struct Sprite {
+    pub registry_idx: usize,
+    pub offset: u8,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct AnimTimeline(pub Vec<ArrayVec<KeyFrame, TEXTURE_ARRAY_SIZE>>);
 
 impl AnimTimeline {
-    pub fn new(key_frames: impl Iterator<Item = KeyFrame>) -> Self {
-        let mut vec = ArrayVec::<KeyFrame, TEXTURE_ARRAY_SIZE>::new();
-
-        for (i, k) in key_frames.enumerate() {
-            vec.insert(i, k);
-        }
-
-        Self(vec)
-    }
     /// elapsed = time since animation began (sec)
     /// anim_duration = duration of one animation cycle (sec)
-    pub fn current_frame(&self, strip: Range<usize>, elapsed: f32) -> u8 {
-        let last_frame_index = strip.clone().last().unwrap();
-        let anim_duration = self.0.get(last_frame_index).unwrap().time;
+    pub fn current_frame(&self, anim_id: u8, elapsed: f32) -> u8 {
+        let last_frame_index = self.0.get(anim_id as usize).unwrap().len() - 1;
+        let anim_duration = self
+            .0
+            .get(anim_id as usize)
+            .unwrap()
+            .get(last_frame_index)
+            .unwrap()
+            .time;
 
         // dt = how far into animation cycle (sec)
         // so we can find what frame should be playing
         let dt = elapsed % anim_duration;
         let mut frame = 0;
-        for f in self.0[strip].iter() {
+        for (i, f) in self.0.get(anim_id as usize).unwrap().iter().enumerate() {
             if dt < f.time {
-                frame = f.index;
+                frame = i;
                 break;
             }
         }
-        frame
+
+        let start = self.0[0..anim_id as usize].iter().flatten().count();
+
+        (frame + start) as u8
     }
 }
 
 impl Sprite {
-    pub fn new(id: usize) -> Self {
-        Self { id, frame_id: 0 }
+    pub fn new(start: usize) -> Self {
+        Self {
+            registry_idx: start,
+            offset: 0,
+        }
     }
 }
